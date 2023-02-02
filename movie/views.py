7b7050acaca42c01e0805acf5836097b65ef748e
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2023-02-02 17:25:37 +0300

    add movie database

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # def home(request):
 #     return HttpResponse('<h1> Welcome Home </h1>')
@@ -11,9 +12,14 @@ def about(request):
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'home.html', {
         'name': 'Erick Mutwiri',
-        'searchTerm': searchTerm
+        'searchTerm': searchTerm,
+        'movies': movies
     })
 
 def signup(request):
